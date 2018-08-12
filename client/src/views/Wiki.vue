ef4fb8b2d2c29e13aa39e89f4500b76575bbcd10
laosb <laosb@lasteam.net>
2018-08-12 00:38:51 -0700

    Basic template functionality.

--- client/src/views/Wiki.vue
+++ client/src/views/Wiki.vue
@@ -1,19 +1,33 @@
 <template>
-  <wiki-partial
-    :wiki="wiki"
-    :top-level="true"
-  ></wiki-partial>
+  <component :is="template">
+    <wiki-partial
+      :wiki="wiki"
+      :top-level="true"
+    ></wiki-partial>
+  </component>
 </template>
 <script>
 import Wiki from '@/engine/Wiki'
 
 import WikiPartial from '@/components/WikiPartial'
 
+const templates = {
+  'wiki': () => import('@/templates/Wiki'),
+  'full-width': () => import('@/templates/FullWidth'),
+  'home': () => import('@/templates/Home')
+}
+
 export default {
-  components: { WikiPartial },
+  components: Object.assign({ WikiPartial }, templates),
   asyncComputed: {
     wiki () {
       return Wiki.getByScopeAndName(this.$route.params.scope, this.$route.params.name)
+    }
+  },
+  computed: {
+    template () {
+      if (this.wiki) return templates[this.wiki.template] || 'wiki'
+      return 'wiki'
     }
   },
   metaInfo () {
